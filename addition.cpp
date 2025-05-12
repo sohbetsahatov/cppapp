8e3fc70e5af7ecaf4558ad6c73b754d275f0a1ad
Sohbet Sahatov <sahatovdoubles@gmail.com>
2025-05-12 23:09:40 +0800

    swap the value of two variables

--- addition.cpp
+++ addition.cpp
@@ -4,9 +4,12 @@ using namespace std;
 
 int main()
 {
-    cout << 1 + 2 << endl;
-
-    system("PAUSE");
-
+    int a = 5;
+    int b = 8;
+    int temp = a;
+    a = b;
+    b = temp;
+    cout<< a<<endl;
+    system("pause");
     return 0;
 }
