9b80862f6c0dd457ee65a7d5f184a696664f938b
Sohbet Sahatov <sahatovdoubles@gmail.com>
2025-05-24 15:17:55 +0800

    Mathematical_expressions

--- addition.cpp
+++ addition.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 
-using namespace std;
+int main() {
+    int a = 3;
+    a--;
+    std:: cout << a;
 
-int main()
-{
-    int a = 5;
-    int b = 8;
-    int temp = a;
-    a = b;
-    b = temp;
-    cout<< a<<endl;
-    system("pause");
-    return 0;
+    system("PAUSE");
+
+    return 0; 
 }
