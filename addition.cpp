8448e290cb5d9d861bc0d987a064a759e9ea8067
Sohbet Sahatov <sahatovdoubles@gmail.com>
2025-05-24 16:58:29 +0800

    Calculate Fahrenheit to Calseus

--- addition.cpp
+++ addition.cpp
@@ -3,15 +3,11 @@
 using namespace std;
 
 int main() {
-    int sales = 95000;
-    double state_tax = sales * 0.04;
-    double county_tax = sales * 0.02; 
-    cout << "State Tax = " << state_tax << endl;
-    cout << "County Tax = " << county_tax <<endl;
-    int sum_payment = state_tax + county_tax;
-    int galan_pul = sales - sum_payment;
-    cout<< "Galan Pul = " << galan_pul;
-
+    cout << " Fahrenheit - i giriz  ";
+    double fahrenheit;
+    cin >> fahrenheit;
+    double calseus = (fahrenheit - 32) * 5 / 9;
+    cout <<" " << calseus;
     system("PAUSE");
 
     return 0; 
