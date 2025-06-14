6adc295118ca95ee82d6f73c16d61dc86d4b6177
Sohbet Sahatov <sahatovdoubles@gmail.com>
2025-06-14 20:38:13 +0800

    area of circle

--- addition.cpp
+++ addition.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int main() {
-    cout << " Fahrenheit - i giriz  ";
-    double fahrenheit;
-    cin >> fahrenheit;
-    double calseus = (fahrenheit - 32) * 5 / 9;
-    cout <<" " << calseus;
+   
+    cout<<"enter radius: ";
+    double radius;
+    cin>> radius;
+    const double pi = 3.14;
+    double result = pi*pow(radius,2);
+    cout<< result; 
+
     system("PAUSE");
 
     return 0; 
