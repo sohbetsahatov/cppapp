80b816ca0f13b7d46047f4b7198b3c48de7e0675
Sohbet Sahatov <sahatovdoubles@gmail.com>
2025-06-29 18:37:40 +0800

    working with numbers

--- addition.cpp
+++ addition.cpp
@@ -4,12 +4,9 @@ using namespace std;
 
 int main() {
    
-    cout<<"enter radius: ";
-    double radius;
-    cin>> radius;
-    const double pi = 3.14;
-    double result = pi*pow(radius,2);
-    cout<< result; 
+   int number = 0xFFB;
+
+   cout<< number;
 
     system("PAUSE");
 
