4b15a4dfbccb99b8b20d6f3d7eadd9aa65db0ac0
Sohbet Sahatov <sahatovdoubles@gmail.com>
2025-07-16 15:38:47 +0800

    size_and_limits

--- addition.cpp
+++ addition.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
-#include <iomanip>
+#include <limits>
 
 using namespace std;
 
 int main() {
    
-   cout << left << setw(15) << "Product" << setw(15) << "Price" << setw(15) << "Discount" << setw(15) << "In Stock" << setw(15) << "Final Price" << endl
-        << fixed << setw(15) <<"Laptop"<< setw(15) << "1500.00" << setw(15) << "10" << setw(15) << "Yes" << setw(15) << "1350.00" <<endl
-        << setw(15) << "Keyboard" << setw(15) << "80.00" << setw(15) << "5" << setw(15) << "No" << setw(15) << "76.00" <<endl
-        << setw(15) << "Monitor" << setw(15) << "300.00" << setw(15) << "15" << setw(15) << "Yes" << setw(15) << "255.00" << endl;
-        system("PAUSE");
+    int number = numeric_limits<int>::max();
+    number++;
+    cout << number;
 
+        system("PAUSE");
 
     return 0; 
 }
