0c2f8d0d13bbebbbc8fb67b5bb0eb449664844df
Sohbet Sahatov <sahatovdoubles@gmail.com>
2025-07-16 16:47:03 +0800

    A program asks some questions

--- addition.cpp
+++ addition.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
-#include <limits>
 
 using namespace std;
 
 int main() {
    
-    int number = numeric_limits<int>::max();
-    number++;
-    cout << number;
+    string street;
+        cout << "Enter your Street: ";
+        getline(cin, street);
+
+    string city;
+        cout << "Enter your city: ";
+            getline(cin, city);
+    
+    string state;
+        cout << "Enter your State: ";
+            getline(cin, state);
+
+    string zipcode;
+        cout << "Enter your zipcode: ";
+            getline(cin, zipcode);
+            
+        cout << street << endl;
+        cout << city <<", " << state <<", " << zipcode << endl;
+
 
         system("PAUSE");
 
