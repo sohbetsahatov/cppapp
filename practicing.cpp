f331584e106696432ecb4a1c74de1f4f268bf933
Sohbet Sahatov <sahatovdoubles@gmail.com>
2025-08-25 03:04:35 +0800

    4-nji amalyn jogaby

--- practicing.cpp
+++ practicing.cpp
@@ -2,22 +2,13 @@
 
 using namespace std;
 
-    int main(){
-    cout << "Enter three-digit number: ";
-    int threeDigitnumber;
-    cin >> threeDigitnumber;
+int main() {
+    string name;
+    cout << "Enter name: ";
+    cin >> name;
     
-    if(threeDigitnumber > 99 && threeDigitnumber < 1000){
-        int firstdigit = threeDigitnumber / 100;
-        int takingSecondDigit = threeDigitnumber % 100;
-        int secondDigit = takingSecondDigit / 10;
-        int thirdDigit = takingSecondDigit % 10;
-    
-        cout << "Sum of digits = " << firstdigit + secondDigit + thirdDigit << endl;
-    }else{
-        cout << "This number is not three-digit" << endl;
-    }
-    
+    cout << "Hello " << name << endl;
+
 
     system("PAUSE");
     return 0;
