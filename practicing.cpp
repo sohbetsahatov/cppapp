26aaa1801aa2fda7fac0c85410c18c5df61a268a
Sohbet Sahatov <sahatovdoubles@gmail.com>
2025-08-27 19:08:36 +0800

    7-nji amalyn jogaby

--- practicing.cpp
+++ practicing.cpp
@@ -7,13 +7,15 @@ int main() {
     int number;
     cin >> number;
     if(number > 9 && number < 100){
-        cout << number / 10 << endl;
-        cout << number % 10 << endl;
+        int x = number / 10;
+        int y = number % 10;
+        int sum = x + y;
+        cout << "Sum of digits = " << sum << endl;
+        int multiple = x * y;
+        cout << "multiple of digits = " << multiple << endl;
     }else{
-        cout << "Error! This number is not two-digit number! " << endl;
+        cout << "Error! This is not two-digit number! " << endl;
     }
-    
-
 
     system("PAUSE");
     return 0;
