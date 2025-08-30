27c036dfac96178a17d23b3a1c514ab31f7327af
Sohbet Sahatov <sahatovdoubles@gmail.com>
2025-08-30 22:43:29 +0800

    exercise 24-26

---
+++ exercise/exercise 25.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+    const int startingDay = 4;
+
+int main(){
+
+    cout << "Enter a day between 1-365: ";
+    int k;
+    cin >> k;
+    if(k > 0 && k < 366){
+        int day = (startingDay + (k -1)) % 7;
+    
+            if (day == 0){
+                cout << "It's Sunday. " << endl;
+            }else if(day == 1){
+                cout << "It's Monday. " << endl;
+            }else if(day == 2){
+                cout << "It's Tuesday. " << endl;
+            }else if(day == 3){
+                cout << "It's Wednesday. " << endl;
+            }else if(day == 4){
+                cout << "It's Thursday. " << endl;
+            }else if(day == 5){
+                cout << "It's Friday. " << endl;
+            }else if(day == 6){
+                cout << "It's Saturday. " << endl;
+            }
+    }else{
+        cout << "It's not between 1-365." << endl;
+    }
+    
+    
+    system("PAUSE");
+    return 0;
